4fa5494683990b5e289a3fff67ca8e2f5b0241cc
Jon Ribeiro <contact@jonathas.com>
2018-07-21 13:51:45 +0200

    Implemented a TypedQuery

--- financas/src/main/java/br/com/caelum/financas/teste/TesteFuncoesJPQL.java
+++ financas/src/main/java/br/com/caelum/financas/teste/TesteFuncoesJPQL.java
@@ -1,9 +1,11 @@
 package br.com.caelum.financas.teste;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.Query;
+import javax.persistence.TypedQuery;
 
 import br.com.caelum.financas.modelo.Conta;
 import br.com.caelum.financas.modelo.TipoMovimentacao;
@@ -28,14 +30,19 @@ public class TesteFuncoesJPQL {
 		
 		System.out.println("A soma é: " + soma);
 		
-		jpql = "SELECT avg(m.valor) FROM Movimentacao m WHERE m.conta = :pConta AND m.tipo = :pTipo";
-		query = manager.createQuery(jpql);
-		query.setParameter("pConta", conta);
-		query.setParameter("pTipo", TipoMovimentacao.SAIDA);
 		
-		Double media = (Double) query.getSingleResult();
+		jpql = "SELECT avg(m.valor) FROM Movimentacao m WHERE m.conta = :pConta AND m.tipo = :pTipo "
+				+ "group by day(m.data), month(m.data), year(m.data)";
 		
-		System.out.println("A media é: " + media);
+		TypedQuery<Double> myQuery = manager.createQuery(jpql, Double.class);
+		myQuery.setParameter("pConta", conta);
+		myQuery.setParameter("pTipo", TipoMovimentacao.SAIDA);
+		
+		List<Double> medias = myQuery.getResultList();
+		
+		System.out.println("A media é: " + medias.get(0));
+		System.out.println("A media é: " + medias.get(1));
+		
 		
 		jpql = "SELECT max(m.valor) FROM Movimentacao m WHERE m.conta = :pConta";
 		query = manager.createQuery(jpql);
